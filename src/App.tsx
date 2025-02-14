bf190729149e4c393d2e4cff5152d784a6ed422c
Zombox <ghostzb@outlook.com>
2025-02-14 14:26:44 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex h-screen w-full justify-center items-center">
+      <h1 className="text-5xl font-bold">Virtualized List Component</h1>
+    </div>
+  )
+}
+
+export default App
