49e36c8712307c0f5240594a2d4142b0e801377e
Zombox <ghostzb@outlook.com>
2025-02-16 17:01:41 +0700

    fix min-h-screen

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,10 +4,10 @@ import NavigationBar from "./navbar/NavigationBar"
 
 function Layout() {
   return (
-    <div className="relative z-0 flex h-screen w-full flex-col items-center justify-center">
+    <div className="relative z-0 flex min-h-screen w-full flex-col items-center justify-center">
       <NavigationBar />
       {/* <Backdrop/> */}
-      <main className="w-full max-w-7xl flex-grow p-6">
+      <main className="w-full h-full max-w-7xl flex-grow p-6">
         <Outlet />
       </main>
     </div>
