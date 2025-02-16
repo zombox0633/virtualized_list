253454a3c87a5e177880fd5a99462eb5e3a19634
Zombox <ghostzb@outlook.com>
2025-02-16 17:03:22 +0700

    add table to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,30 @@
+import FormNewProduct from "../components/table/FormNewProduct"
+import Table from "../components/table/Table"
+import useDownloadCSV from "../hook/useDownloadCSV"
 import useGetAllProduct from "../hook/useGetAllProduct"
 
 function HomePage() {
-  // const {data} = useGetAllProduct()
+  const { downloadCSV } = useDownloadCSV()
+  const { data } = useGetAllProduct()
 
-  // console.log(data);
-
-  return <div>HomePage</div>
+  return (
+    <div className="relative h-full w-full">
+      <section>
+        <div className="my-2 flex items-center justify-between">
+          <FormNewProduct />
+          <button
+            type="button"
+            onClick={() => downloadCSV(data)}
+            disabled={!data || data.length === 0}
+            className="h-12 w-auto bg-[#17c964] px-4 py-2 font-bold text-white hover:bg-[#17c964]/80 disabled:bg-[#17c964]/70"
+          >
+            Download CSV
+          </button>
+        </div>
+        <Table products={data} />
+      </section>
+    </div>
+  )
 }
 
 export default HomePage
